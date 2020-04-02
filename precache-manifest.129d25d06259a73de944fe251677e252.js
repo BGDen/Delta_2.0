816f6afdb7dd111395e21a407d9e009c6697261c
Den <fentau.line@gmail.com>
2020-04-02 12:49:06 +0300

    Updates

---
+++ precache-manifest.129d25d06259a73de944fe251677e252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bebc7ce780e529ae393febe39a505b1",
+    "url": "/Delta_2.0/index.html"
+  },
+  {
+    "revision": "66855572315d24f17b22",
+    "url": "/Delta_2.0/static/css/main.f052432d.chunk.css"
+  },
+  {
+    "revision": "7437f43453429071d368",
+    "url": "/Delta_2.0/static/js/2.1e00dd89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Delta_2.0/static/js/2.1e00dd89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66855572315d24f17b22",
+    "url": "/Delta_2.0/static/js/main.87c8d15d.chunk.js"
+  },
+  {
+    "revision": "a20ed246d3242963828f",
+    "url": "/Delta_2.0/static/js/runtime-main.7caa3e11.js"
+  }
+]);
