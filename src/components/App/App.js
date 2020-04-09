28bb1ed3a3d0e178d79ee2c227c45b0635222443
Den <fentau.line@gmail.com>
2020-04-09 15:20:45 +0300

    init commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Card} from '../Card/Card';
 import {Navigation} from '../Navbar/Navbar';
-import {Dropdown} from 'primereact/dropdown';
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
+
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+// import {Dropdown} from 'primereact/dropdown';
+// import 'primereact/resources/themes/nova-light/theme.css';
+// import 'primereact/resources/primereact.min.css';
 
 // import ReactDOM from 'react-dom';
 
@@ -14,13 +18,17 @@ class App extends React.Component{
     this.state = {
       data : [],
       searchData : [],
-      name1: null
+
+      newArr : []
+      
+      // brand: null
     }
     this.link = 'https://spreadsheets.google.com/feeds/list/1JIuV-NlPAC93DTqVOdh50MuFSH8h0o8KnphSQ2YxTko/od6/public/values?alt=json'
     this.loadCards();
 
-    this.onNameChange = this.onNameChange.bind(this);
+    // this.onNameChange = this.onNameChange.bind(this);
     // this.unique = this.unique.bind(this);
+    // this.filteringCards = this.filteringCards.bind(this);
   }
 
   loadCards(){
@@ -37,7 +45,7 @@ class App extends React.Component{
         }
       });
       console.log(data);
-      this.setState({filterData : data});
+      // this.setState({filterData : data});
       this.setState({searchData : data});
       this.setState({data});
     });
@@ -50,14 +58,20 @@ class App extends React.Component{
   }
 
   filteringCards(){
-    const filterData = [...new Set(this.data)];
-    this.setState({filterData})
-    // console.log(this.setState);
+    const newArr = [];
+    this.data.forEach(el=> (!newArr.includes(el.name))? newArr.push(el.name): false);
+    this.setState({newArr});
+    console.log(newArr);
   }
 
-  onNameChange(e){
-    this.setState({name1: e.value});
-  }
+    // const filterData = [...new Set(this.data.key)];
+    // this.setState({filterData})
+    // console.log(this.setState);
+  // }
+
+  // onNameChange(e){
+  //   this.setState({brand: e.value});
+  // }
 
   // unique(data) {
   //   let result = [];
@@ -77,11 +91,16 @@ class App extends React.Component{
         <div className='navigation' onChange={this.searchingCards.bind(this)}>
           <Navigation/>
         </div>
+
+        <Dropdown options = {this.state.newArr} onChange = {this._onSelect} value = {this.state.newArr[0]} placeholder = "Выберите параметр" />
         
-        <div className="content-section implementation">
-              <Dropdown value={this.state.name1} options={this.state.filterData} ariaLabel="Test" onChange={this.onNameChange} placeholder="Select" optionLabel="name" style={{width: '12em'}}/>
-              <div style={{marginTop: '.5em'}}>{this.state.filterData ? 'Selected Tag: ' + this.state.filterData.name : 'No Tag selected'}</div>
-        </div>
+        {/* <div className="content-section implementation">
+              <Dropdown value={this.state.brand} options={this.state.filterData} ariaLabel="Test" onChange={this.onNameChange} placeholder="Select" optionLabel="name" style={{width: '12em'}}/> */}
+
+              {/* <Dropdown optionLabel="name" value={this.state.brand} options={this.state.filterData} onChange={(e) => {this.setState({data: e.value})}} placeholder="Select a City"/> */}
+
+              {/* <div style={{marginTop: '.5em'}}>{this.state.filterData ? 'Selected Tag: ' + this.state.filterData.name : 'No Tag selected'}</div>
+        </div> */}
 
         <div className='render_all_card'>
               {this.state.searchData.map((msg)=> <Card data={msg} key={msg.id} />)}
